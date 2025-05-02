dbb69f9774403c4d6931dbe45f4428dd9eb4fdab
aaaa45 <enenyuji@gmail.com>
2025-05-02 14:29:57 +0900

    最初のコミット

---
+++ src/tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    content: [
+      "./src/**/*.{html,js,jsx}",
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
+// tailwind.config.js
+module.exports = {
+    content: ['./src/**/*.{js,jsx,ts,tsx,html}'],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
+  
