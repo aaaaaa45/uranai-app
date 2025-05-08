f704838077628a1bdedfdc8bd5aea470ed28e1da
aaaa45 <enenyuji@gmail.com>
2025-05-08 11:19:54 +0900

    5/8午前の変更

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,35 @@
-// src/App.jsx
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+// // src/App.jsx
+// import React from 'react';
+// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+// import Home from './pages/Home';
+// import About from './pages/About';
+// import NotFound from './pages/NotFound';
+// import Fortune from './Fortune';
+
+// function App() {
+//   return (
+//     <Router>
+//       <Routes>
+//         <Route path="/" element={<Fortune />} />
+//         <Route path="/about" element={<About />} />
+//         <Route path="*" element={<NotFound />} />
+//       </Routes>
+//     </Router>
+//   );
+// }
+
+// export default App;
+
+import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
-import NotFound from './pages/NotFound';
-import Fortune from './Fortune';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Fortune />} />
-        <Route path="/about" element={<About />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+    </Routes>
   );
 }
 
